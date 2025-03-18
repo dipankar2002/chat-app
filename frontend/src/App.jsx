881142943424b415e4eea124d7c002f5e277cddc
dipankar2002 <dipankarganguly15@gmail.com>
2025-03-18 22:52:31 +0530

    adding all the pages and compo in a react-router-dom tag and impliment some codes and create a loading component using lucide-react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,42 @@
 
+import { Router, Route, Routes, Navigate } from "react-router-dom"
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingPage from "./pages/SettingPage";
+import ProfilePage from "./pages/ProfilePage";
+import Navbar from "./components/Navbar";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+import { Loader } from 'lucide-react';
+
 function App() {
-  return <>
-  </>
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  useEffect(() => {
+    checkAuth();
+  },[]);
+  console.log(authUser);
+
+  if(isCheckingAuth && !authUser) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={authUser ? <HomePage />:<Navigate to="login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/"/>} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/"/>} />
+        <Route path="/settings" element={<SettingPage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage />:<Navigate to="login" />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App
