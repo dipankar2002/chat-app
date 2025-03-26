d6e60cc686fdbccfe090d3023ecef1e208e8e23a
dipankar2002 <dipankarganguly15@gmail.com>
2025-03-26 10:34:03 +0530

    add cors limit 5mb

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,9 @@ dotenv.config();
 const port = process.env.PORT;
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));  // Adjust the limit as needed
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
+
 app.use(cookeiParser());
 app.use(cors({
     origin: "http://localhost:5173", // Replace with your frontend URL
