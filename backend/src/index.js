d2cf9ca9e0a5f3ddc872464d8faa6e4b745b3939
dipankar2002 <dipankarganguly15@gmail.com>
2025-04-22 12:40:37 +0530

    feat: update base URL configuration for development and production environments

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,11 @@ import userRoute from './routes/user.router.js';
 import messageRoute from './routes/message.router.js';
 import { connectDB } from './lib/db.js';
 import { app, serverInstance } from './lib/socket.js';
+import path from 'path';
 
 dotenv.config();
 const port = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: '5mb' }));  // Adjust the limit as needed
 // app.use(express.urlencoded({ extended: true, limit: '5mb' }));
@@ -22,6 +24,12 @@ app.use(cors({
 app.use('/api/v1/auth', userRoute);
 app.use('/api/v1/message', messageRoute);
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 serverInstance.listen(port, ()=>{
     console.log(`Server start`);
     connectDB();
