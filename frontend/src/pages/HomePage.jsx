8bdacea69a6979da4612eb58148777940471c94c
dipankar2002 <dipankarganguly15@gmail.com>
2025-07-16 21:59:38 +0530

    make the sidebar close on phone screen when any user chat section is open or any user is selected

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -12,7 +12,7 @@ const HomePage = () => {
       <div className='flex items-center justify-center pt-16'>
         <div className='bg-base-200 rounded-lg shadow-xl w-full h-[calc(100vh-4rem)]'>
           <div className='flex h-full rounded-lg overflow-hidden'>
-            <Sidebar />
+            { !selectedUser && <Sidebar /> }
             { selectedUser ? <ChatSection /> : <EmptyChat /> }
           </div>
         </div>
